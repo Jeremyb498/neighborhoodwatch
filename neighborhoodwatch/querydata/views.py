566a98969b10bfb1b984049c877e5b9a347006f5
Esteban Medero <estebanmedero@ssrb-vpn1-3-181.vpn.ufl.edu>
2023-04-19 17:31:50 -0400

    parsed table selection

--- neighborhoodwatch/querydata/views.py
+++ neighborhoodwatch/querydata/views.py
@@ -12,20 +12,17 @@ connection = oracledb.connect(
 
 print("Successfully connected to Oracle Database")
 
-cursor = connection.cursor()
-
-crimes = "otherproperty"
-
-# Now query the rows back
-cursor.execute("select * from " + crimes)
-columnnames = []
-
-for item in cursor.description:
-    columnnames.append(item[0])
-
-res = cursor.fetchall()
-
 def index(request):
+    cursor = connection.cursor()
+
+    # Now query the rows back
+    cursor.execute("SELECT table_name FROM user_tables")
+    columnnames = []
+
+    for item in cursor.description:
+        columnnames.append(item[0])
+
+    res = cursor.fetchall()
     query = res
     template = loader.get_template("querydata/index.html")
     context = {
@@ -33,4 +30,29 @@ def index(request):
         "columns" : columnnames
     }
     return HttpResponse(template.render(context, request))
+
+def tableSelection(request):
+
+    cursor = connection.cursor()
+    columnnames = []
+
+    selectedTables = request.POST.getlist('selection')
+    queries = ', '.join(selectedTables)
+
+    cursor.execute("select * from " + queries + " where agency.statename = 'COLORADO'")
+
+    for item in cursor.description:
+        columnnames.append(item[0])
+
+    res = cursor.fetchall()
+    query = res
+    template = loader.get_template("querydata/index.html")
+    context = {
+        "query" : query,
+        "columns" : columnnames
+    }
+    return HttpResponse(template.render(context, request))
+
+
+
 
